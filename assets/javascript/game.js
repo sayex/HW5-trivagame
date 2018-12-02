4abcb47d137c31304a5ee81e158467b13aa1477c
sayex <elsayer@msn.com>
2018-12-02 00:57:09 -0700

    adding questoins

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,23 +4,29 @@ $(document).ready(function () {
 
     var questionsObject = {
         "1": {
-            q: "What Color is the Sky?",
-            possibleAnswers: ["blue", "green", "yellow", "red"],
-            answer: "blue",
-            imageLocation: "assets/images/happy1.gif"
+            q: "Where is the longest lasting lightbulb currently lit?",
+            possibleAnswers: ["New York", "California", "London", "Paris"],
+            answer: "California",
+            imageLocation: "assets/images/longestlight.jpg"
 
         },
         "2": {
-            q: "Who said the answer is 42?",
-            possibleAnswers: ["Douglas Adams", "Hurbert Walker", "Siri", "Mother Terisa"],
-            answer: "Douglas Adams",
-            imageLocation: "assets/images/tenor.gif"
+            q: "Who invented the first lightbuld?",
+            possibleAnswers: ["Henry Ford", "Humphry Davy", "Nicola Tesla", "Thomas Edison"],
+            answer: "Humphry Davy",
+            imageLocation: "assets/images/humphydavy.jpg"
         },
         "3": {
-            q: "Did Someone Say test?",
-            possibleAnswers: ["Yes", "No", "Maybe", "Kinda"],
-            answer: "Yes",
-            imageLocation: "assets/images/tenor.gif"
+            q: "What hosted the first event that used lighting and clinched AC electricity?",
+            possibleAnswers: ["Utah Becomes a State", "Chicago World's Fair", "Effile Tower Built", "Nobel Prize Created"],
+            answer: "Chicago World's Fair",
+            imageLocation: "assets/images/worldsfair.jpg"
+        },
+        "4": {
+            q: "How many lights on a house is currently the world record?",
+            possibleAnswers: ["601,736", "1.1b", "204,451", "86,923"],
+            answer: "601,736",
+            imageLocation: "assets/images/mostlightsonhouse.jpg"
         }
 
     };
@@ -42,8 +48,8 @@ $(document).ready(function () {
     function gameLoad() {
         var startButton = $("<h1>");
         startButton.addClass("startButton");
-        startButton.text("Start")
-        $("#answers").append(startButton)
+        startButton.text("Start");
+        $("#answers").append(startButton);
         $(".startButton").on("click", function () {
             emptyText();
             displayQuestion(qCounter);
@@ -53,87 +59,90 @@ $(document).ready(function () {
 
     // to clear the screen
     function emptyText() {
-        $("#answers").empty()
-        $(".question").empty()
+        $("#answers").empty();
+        $(".question").empty();
     }
 
     //what happens when guessed correctly
     function correctGuess() {
-        guessedCorrect++
-        stop()
-        timeLeft = timeReset
-        $("#timeLeft").html(timeLeft)
-        emptyText()
-        $(".question").text("Correct!!")
-        var imgDisplay = $("<img>")
-        imgDisplay.attr("src", questionImg)
+        guessedCorrect++;
+        stop();
+        timeLeft = timeReset;
+        $("#timeLeft").html(timeLeft);
+        emptyText();
+        $(".question").text("Correct!!");
+        var imgDisplay = $("<img>");
+        imgDisplay.attr("src", questionImg);
         $("#answers").append(imgDisplay);
 
     }
 
     //what happens when incorrectly guessed
     function incorrectGuess() {
-        guessedInncorect++
-        stop()
-        timeLeft = timeReset
-        $("#timeLeft").html(timeLeft)
-        emptyText()
-        $(".question").text("Incorrect!!")
-        $("#answers").html("<h2>Correct answer was: " + correctAnswer + "</h2>")
+        guessedInncorect++;
+        stop();
+        timeLeft = timeReset;
+        $("#timeLeft").html(timeLeft);
+        emptyText();
+        $(".question").text("Incorrect!!");
+        $("#answers").html("<h2>Correct answer was: " + correctAnswer + "</h2>");
+        var imgDisplay = $("<img>");
+        imgDisplay.attr("src", questionImg);
+        $("#answers").append(imgDisplay);
     }
 
     //what happens when the timer reaches zero
     function timmerUp() {
-        noGuessAnswers++
-        stop()
-        timeLeft = timeReset
-        $("#timeLeft").html(timeLeft)
-        $("#answers").empty()
-        $(".question").empty()
-        qCounter++
+        noGuessAnswers++;
+        stop();
+        timeLeft = timeReset;
+        $("#timeLeft").html(timeLeft);
+        $("#answers").empty();
+        $(".question").empty();
+        qCounter++;
         setTimeout(function () {
             displayQuestion(qCounter)
-        }, 3000)
-        $(".question").text("Times Up!!")
-        $("#answers").html("<h2>Correct answer was: " + correctAnswer + "</h2>")
+        }, 3000);
 
+        $(".question").text("Times Up!!");
+        $("#answers").html("<h2>Correct answer was: " + correctAnswer + "</h2>");
+        var imgDisplay = $("<img>");
+        imgDisplay.attr("src", questionImg);
+        $("#answers").append(imgDisplay);
 
-    }
+    };
 
     //create function to end the game
 
     function gameOver() {
-        $(".question").text("Game Over")
-        $("#answers").append("<p> Correctly Guessed: " + guessedCorrect + "</p>")
-
-        $("#answers").append("<p> Incorreclty Guessed: " + guessedInncorect + "</p>")
-
-        $("#answers").append("<p> Unanswered Questions: " + noGuessAnswers + "</p>")
+        $(".question").text("Game Over");
+        $("#answers").append("<p> Correctly Guessed: " + guessedCorrect + "</p>");
+        $("#answers").append("<p> Incorreclty Guessed: " + guessedInncorect + "</p>");
+        $("#answers").append("<p> Unanswered Questions: " + noGuessAnswers + "</p>");
 
         var startButton = $("<h1>");
         startButton.addClass("startButton");
-        startButton.text("Play Again?")
-        $("#answers").append(startButton)
+        startButton.text("Play Again?");
+        $("#answers").append(startButton);
         $(".startButton").on("click", function () {
             $("#answers").empty();
-            guessedCorrect = 0
-            guessedInncorect = 0
-            qCounter = 1
-            noGuessAnswers = 0
+            guessedCorrect = 0;
+            guessedInncorect = 0;
+            qCounter = 1;
+            noGuessAnswers = 0;
             displayQuestion(qCounter);
-        })
-    }
+        });
+    };
 
     // create function to reach inot object, pull out questions and answers and to update the display with the information
 
     function displayQuestion(x) {
         emptyText();
-        if (qCounter === 4) {
+        if (qCounter === 11) {
             gameOver()
 
         } else {
-            var currentQuestion = questionsObject[x].q
-
+            var currentQuestion = questionsObject[x].q;
 
             var answers = questionsObject[x].possibleAnswers;
 
@@ -144,33 +153,33 @@ $(document).ready(function () {
             for (i = 0; i < answers.length; i++) {
                 var answersDiv = $("<div>");
                 var h1 = $("<h1>");
-                h1.addClass("answer-generated")
-                h1.val(answers[i])
-                h1.text(answers[i])
+                h1.addClass("answer-generated");
+                h1.val(answers[i]);
+                h1.text(answers[i]);
 
-                answersDiv.append(h1)
+                answersDiv.append(h1);
 
-                $("#answers").append(answersDiv)
+                $("#answers").append(answersDiv);
 
             }
-            $(".question").text(currentQuestion)
-            countDownTimer()
+            $(".question").text(currentQuestion);
+            countDownTimer();
 
             $(".answer-generated").on("click", function () {
 
                 var answerClicked = $(this).val();
                 if (answerClicked === correctAnswer) {
-                    qCounter++
+                    qCounter++;
                     correctGuess();
                     setTimeout(function () {
                         displayQuestion(qCounter)
-                    }, 3000)
+                    }, 3000);
                 } else {
-                    qCounter++
-                    incorrectGuess()
+                    qCounter++;
+                    incorrectGuess();
                     setTimeout(function () {
                         displayQuestion(qCounter)
-                    }, 3000)
+                    }, 3000);
 
                 }
             })
