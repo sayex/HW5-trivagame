1a81f7b975d334348627ef7503e6914aef8ff6c7
sayex <elsayer@msn.com>
2018-12-01 23:42:12 -0700

    added comments and document.ready

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,196 +1,213 @@
 //create an object that holds all questions and answers
 
-var questionsObject = {
-    "1": {
-        q: "What Color is the Sky?",
-        possibleAnswers: ["blue", "green", "yellow", "red"],
-        answer: "blue",
-        imageLocation: "assets/images/happy1.gif"
+$(document).ready(function () {
 
-    },
-    "2": {
-        q: "Who said the answer is 42?",
-        possibleAnswers: ["Douglas Adams", "Hurbert Walker", "Siri", "Mother Terisa"],
-        answer: "Douglas Adams",
-        imageLocation: "assets/images/tenor.gif"
-    },
-    "3": {
-        q: "Did Someone Say test?",
-        possibleAnswers: ["Yes", "No", "Maybe", "Kinda"],
-        answer: "Yes",
-        imageLocation: "assets/images/tenor.gif"
-    }
+    var questionsObject = {
+        "1": {
+            q: "What Color is the Sky?",
+            possibleAnswers: ["blue", "green", "yellow", "red"],
+            answer: "blue",
+            imageLocation: "assets/images/happy1.gif"
 
-};
-
-
-var guessedCorrect = 0;
-var guessedInncorect = 0;
-var noGuessAnswers = 0;
-var timeReset = 10
-var timeLeft = 10;
-var intervalId;
-var qCounter = 1;
-var correctAnswer = "";
-var questionImg = "";
-
-
-function gameLoad() {
-    var startButton = $("<h1>");
-    startButton.addClass("startButton");
-    startButton.text("Start")
-    $("#answers").append(startButton)
-    $(".startButton").on("click", function () {
-        emptyText();
-        displayQuestion(qCounter);
-    })
-
-}
-
-function emptyText() {
-    $("#answers").empty()
-    $(".question").empty()
-}
-
-function correctGuess() {
-    guessedCorrect++
-    stop()
-    timeLeft = timeReset
-    $("#timeLeft").html(timeLeft)
-    emptyText()
-    $(".question").text("Correct!!")
-    var imgDisplay = $("<img>")
-    imgDisplay.attr("src", questionImg)
-    $("#answers").append(imgDisplay);
-
-}
-
-function incorrectGuess() {
-    guessedInncorect++
-    stop()
-    timeLeft = timeReset
-    $("#timeLeft").html(timeLeft)
-    emptyText()
-    $(".question").text("Incorrect!!")
-    $("#answers").html("<h2>correct answer was " + correctAnswer + "</h2>")
-}
-
-function timmerUp() {
-    noGuessAnswers++
-    stop()
-    timeLeft = timeReset
-    $("#timeLeft").html(timeLeft)
-    $("#answers").empty()
-    $(".question").empty()
-    qCounter++
-    setTimeout(function () {
-        displayQuestion(qCounter)
-    }, 3000)
-    $(".question").text("Times Up!!")
-    $("#answers").html("<h2>correct answer was " + correctAnswer + "</h2>")
-
-
-}
-
-function gameOver() {
-    $(".question").text("Game Over")
-    console.log(guessedCorrect);
-    console.log(guessedInncorect);
-    console.log(noGuessAnswers);
-
-    var startButton = $("<h1>");
-    startButton.addClass("startButton");
-    startButton.text("Play Again?")
-    $("#answers").append(startButton)
-    $(".startButton").on("click", function () {
-        $("#answers").empty();
-        guessedCorrect = 0
-        guessedInncorect = 0
-        qCounter = 1
-        noGuessAnswers = 0
-        displayQuestion(qCounter);
-    })
-}
-
-function displayQuestion(x) {
-    emptyText();
-    if (qCounter === 4) {
-        gameOver()
-
-    } else {
-        var currentQuestion = questionsObject[x].q
-
-
-        var answers = questionsObject[x].possibleAnswers;
-
-        correctAnswer = questionsObject[x].answer;
-
-        questionImg = questionsObject[x].imageLocation;
-
-        for (i = 0; i < answers.length; i++) {
-            var answersDiv = $("<div>");
-            var h1 = $("<h1>");
-            h1.addClass("answer-generated")
-            h1.val(answers[i])
-            h1.text(answers[i])
-
-            answersDiv.append(h1)
-
-            $("#answers").append(answersDiv)
-
+        },
+        "2": {
+            q: "Who said the answer is 42?",
+            possibleAnswers: ["Douglas Adams", "Hurbert Walker", "Siri", "Mother Terisa"],
+            answer: "Douglas Adams",
+            imageLocation: "assets/images/tenor.gif"
+        },
+        "3": {
+            q: "Did Someone Say test?",
+            possibleAnswers: ["Yes", "No", "Maybe", "Kinda"],
+            answer: "Yes",
+            imageLocation: "assets/images/tenor.gif"
         }
-        $(".question").text(currentQuestion)
-        countDownTimer()
 
-        $(".answer-generated").on("click", function () {
+    };
 
-            var answerClicked = $(this).val();
-            if (answerClicked === correctAnswer) {
-                qCounter++
-                correctGuess();
-                setTimeout(function () {
-                    displayQuestion(qCounter)
-                }, 3000)
-            } else {
-                qCounter++
-                incorrectGuess()
-                setTimeout(function () {
-                    displayQuestion(qCounter)
-                }, 3000)
+    // create varables for the game
+    var guessedCorrect = 0;
+    var guessedInncorect = 0;
+    var noGuessAnswers = 0;
+    var timeReset = 10
+    var timeLeft = 10;
+    var intervalId;
+    var qCounter = 1;
+    var correctAnswer = "";
+    var questionImg = "";
 
-            }
+    //  create functions for the specific tasks
+
+    //when game loads
+    function gameLoad() {
+        var startButton = $("<h1>");
+        startButton.addClass("startButton");
+        startButton.text("Start")
+        $("#answers").append(startButton)
+        $(".startButton").on("click", function () {
+            emptyText();
+            displayQuestion(qCounter);
         })
 
     }
-}
 
+    // to clear the screen
+    function emptyText() {
+        $("#answers").empty()
+        $(".question").empty()
+    }
 
+    //what happens when guessed correctly
+    function correctGuess() {
+        guessedCorrect++
+        stop()
+        timeLeft = timeReset
+        $("#timeLeft").html(timeLeft)
+        emptyText()
+        $(".question").text("Correct!!")
+        var imgDisplay = $("<img>")
+        imgDisplay.attr("src", questionImg)
+        $("#answers").append(imgDisplay);
 
+    }
 
+    //what happens when incorrectly guessed
+    function incorrectGuess() {
+        guessedInncorect++
+        stop()
+        timeLeft = timeReset
+        $("#timeLeft").html(timeLeft)
+        emptyText()
+        $(".question").text("Incorrect!!")
+        $("#answers").html("<h2>Correct answer was: " + correctAnswer + "</h2>")
+    }
 
-//code a timer for each question
-
-
-function countDownTimer() {
-    clearInterval(intervalId)
-    intervalId = setInterval(decrement, 1000);
-}
-
-function decrement() {
-    timeLeft--;
-    $("#timeLeft").html(timeLeft)
-
-    if (timeLeft === 0) {
-        timmerUp()
-
+    //what happens when the timer reaches zero
+    function timmerUp() {
+        noGuessAnswers++
+        stop()
+        timeLeft = timeReset
+        $("#timeLeft").html(timeLeft)
+        $("#answers").empty()
+        $(".question").empty()
+        qCounter++
+        setTimeout(function () {
+            displayQuestion(qCounter)
+        }, 3000)
+        $(".question").text("Times Up!!")
+        $("#answers").html("<h2>Correct answer was: " + correctAnswer + "</h2>")
 
 
     }
-}
 
-function stop() {
-    clearInterval(intervalId)
-}
+    //create function to end the game
+
+    function gameOver() {
+        $(".question").text("Game Over")
+        $("#answers").append("<p> Correctly Guessed: " + guessedCorrect + "</p>")
+
+        $("#answers").append("<p> Incorreclty Guessed: " + guessedInncorect + "</p>")
+
+        $("#answers").append("<p> Unanswered Questions: " + noGuessAnswers + "</p>")
+
+        var startButton = $("<h1>");
+        startButton.addClass("startButton");
+        startButton.text("Play Again?")
+        $("#answers").append(startButton)
+        $(".startButton").on("click", function () {
+            $("#answers").empty();
+            guessedCorrect = 0
+            guessedInncorect = 0
+            qCounter = 1
+            noGuessAnswers = 0
+            displayQuestion(qCounter);
+        })
+    }
+
+    // create function to reach inot object, pull out questions and answers and to update the display with the information
+
+    function displayQuestion(x) {
+        emptyText();
+        if (qCounter === 4) {
+            gameOver()
+
+        } else {
+            var currentQuestion = questionsObject[x].q
 
 
-gameLoad()
+            var answers = questionsObject[x].possibleAnswers;
+
+            correctAnswer = questionsObject[x].answer;
+
+            questionImg = questionsObject[x].imageLocation;
+
+            for (i = 0; i < answers.length; i++) {
+                var answersDiv = $("<div>");
+                var h1 = $("<h1>");
+                h1.addClass("answer-generated")
+                h1.val(answers[i])
+                h1.text(answers[i])
+
+                answersDiv.append(h1)
+
+                $("#answers").append(answersDiv)
+
+            }
+            $(".question").text(currentQuestion)
+            countDownTimer()
+
+            $(".answer-generated").on("click", function () {
+
+                var answerClicked = $(this).val();
+                if (answerClicked === correctAnswer) {
+                    qCounter++
+                    correctGuess();
+                    setTimeout(function () {
+                        displayQuestion(qCounter)
+                    }, 3000)
+                } else {
+                    qCounter++
+                    incorrectGuess()
+                    setTimeout(function () {
+                        displayQuestion(qCounter)
+                    }, 3000)
+
+                }
+            })
+
+        }
+    }
+
+
+
+
+
+    //code a timer for each question
+
+
+    function countDownTimer() {
+        clearInterval(intervalId)
+        intervalId = setInterval(decrement, 1000);
+    }
+
+    function decrement() {
+        timeLeft--;
+        $("#timeLeft").html(timeLeft)
+
+        if (timeLeft === 0) {
+            timmerUp()
+
+
+
+        }
+    }
+
+    function stop() {
+        clearInterval(intervalId)
+    }
+
+    //game starts at page load
+
+    gameLoad()
+
+});
